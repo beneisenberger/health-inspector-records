d21a3b09aefec49328259932fb452b7644712082
Ben Eisenberger <appsky@Bens-MacBook-Pro.local>
2020-06-29 23:11:58 -0500

    restyling

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,32 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { CartService } from '../cart.service';
-import forSale from '../forSale';
+import { Component, OnInit } from "@angular/core";
+import { CartService } from "../cart.service";
+import forSale from "../forSale";
 
 @Component({
-  selector: 'app-store',
-  templateUrl: './store.component.html',
-  styleUrls: ['./store.component.scss']
+  selector: "app-store",
+  templateUrl: "./store.component.html",
+  styleUrls: ["./store.component.scss"],
 })
 export class StoreComponent implements OnInit {
-
   itemsForSale = forSale;
   selectedItem = [];
 
-  constructor(public cartService: CartService) { }
+  constructor(public cartService: CartService) {}
 
   ngOnInit() {
-    this.getItems()
+    this.getItems();
   }
 
   async getItems() {
     await this.itemsForSale;
-    this.selectedItem.length === this.itemsForSale.length
+    this.selectedItem.length === this.itemsForSale.length;
   }
 
   addToCart(item, product) {
-    if (product.status === 'active') {
+    if (product.status === "active") {
       this.cartService.addItem(item);
     }
   }
-
 }
